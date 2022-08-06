a23d1035b04a23700b5a84f944a9e2675bf6f54a
Gene-94 <yevheniykushnirenko@gmail.com>
2022-08-06 21:30:50 +0100

    got needed elements

--- scrapper.py
+++ scrapper.py
@@ -4,10 +4,15 @@ from bs4 import BeautifulSoup
 req = requests.get("https://www.eventbrite.com/d/online/meditation/")
 
 soup = BeautifulSoup(req.content, "html.parser")
-content = soup.find(class_='eds-event-card-content__primary-content')
+event_card = soup.find(class_='eds-event-card-content__primary-content')
 
-link = content.find(class_='eds-event-card-content__action-link')
-links = link.contents()
-print(links[0])
-content.find(class_='eds-event-card-content__action-link')
-content.find(class_='eds-event-card-content__sub-title')
+link = event_card.find(class_='eds-event-card-content__action-link')
+title = link.h3
+event_date = event_card.find(class_='eds-event-card-content__sub-title')
+
+
+print(link)
+print("-"*20)
+print(title)
+print("-"*20)
+print(event_date)
