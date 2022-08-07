4da9d2cce9daf6d8bd03e5ee2ae4ccbe8b41a786
Gene-94 <yevheniykushnirenko@gmail.com>
2022-08-07 18:05:22 +0100

    sucessfully getting the date into datetime fromat from string

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,6 @@
 import requests 
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 req = requests.get("https://www.eventbrite.com/d/online/meditation/")
 
@@ -11,14 +12,24 @@ event_cards = soup.find_all("article")
 for i in range(0, len(event_cards), 2):
     link = event_cards[i].find(class_='eds-event-card-content__action-link')
     title = link.h3.div.div
-    event_date = event_cards[i].find(class_='eds-event-card-content__sub-title')
     image = event_cards[i].img
+    event_date_string = (event_cards[i].find(class_='eds-event-card-content__sub-title')).text
+    event_date_string = event_date_string.split('(')
+    event_date_str = (event_date_string[0].strip())[:-4]
+    date_offset = (event_date_string[1])[0:6]
+    event_date = datetime.strptime(event_date_str, "%a, %b %d, %Y %I:%M %p")
 
     print(link.get('href'))
     print("-"*20)
     print(title.text) 
     print("-"*20)
-    print(event_date.text)
+    print(event_date_str)
+    print("-"*20)
+    print(date_offset)
+    print("-"*20)
+    print(event_date)
     print("-"*20)
     print(image.get('src'))
     print("\n"+"#"*20,"\n")
+
+print(datetime.now())
